fe5d65a5a41d2e7927cd22f20a5a62b3865ad536
sangram-va <sangram792114@gmail.com>
2025-11-19 00:02:01 +0530

    Print Something in console without semicolon

---
+++ PrintInConsoleWithoutSemiColon.java
@@ -0,0 +1,9 @@
+class PrintInConsoleWithoutSemiColon
+{
+    public static void main(String[] args) {
+        if(System.out.printf("Hello Java Learners now without semicolon it will print output in console") == null)
+        {
+
+        }
+    }
+}
