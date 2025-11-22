ab692153c0704f035c13d8217edb2904b59780aa
sangram-va <sangram792114@gmail.com>
2025-11-22 23:29:54 +0530

    Polymorphism Intro

---
+++ OOPS/Polymorphism.java
@@ -0,0 +1,9 @@
+class Polymorphism
+{
+    // Polymorphism has two types :
+    // compile time and run time polymorphism 
+    // under compile time polymorphism we have examples like method overloading 
+    // shadowing and constructor overloading
+
+    // under run time polymorphism we have examples like method overriding
+}
