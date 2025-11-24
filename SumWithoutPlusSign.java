447dbebaafc7ab21a791929b3ef9d8af46f1336e
sangram-va <sangram792114@gmail.com>
2025-11-24 22:08:10 +0530

    Tricky Question

---
+++ SumWithoutPlusSign.java
@@ -0,0 +1,14 @@
+class SumWithoutPlusSign
+{
+    public static void main(String[] args) {
+        //here we use concept of unary and binary operator 
+        // unary operator means which deals with one operand (operand means data/variable/value)
+        // example : -5 here ' - ' is a unary operator and 5 is operand
+        // similarly binary operator means single operator 2 operands
+        // now add 2 numbers without + operator 
+
+        System.out.println(2- -3); // output 5 
+        // first '-' is binary operator and second is unary which makes 3 as -3 
+        // internally they both addup and acts like sum 
+    }
+}
